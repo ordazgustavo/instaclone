44f7d5a602f036fd6f9e58f939e3e80fce720b98
Gustavo Ordaz <ordazsgustavo@gmail.com>
2019-01-27 18:18:58 -0500

    Load more posts on scroll bottom

--- pages/index.js
+++ pages/index.js
@@ -14,86 +14,119 @@ import userGenerator from '../utils/userGenerator'
 import storiesGenerator from '../utils/storiesGenerator'
 import suggestionsGenerator from '../utils/suggestionsGenerator'
 
-function Home({ posts, userInfo, stories, suggestions }) {
-  return (
-    <section>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Dancing+Script"
-          rel="stylesheet"
-        />
-      </Head>
-      <Header />
-      <Main>
-        <section className="page-wrapper">
-          <div className="posts-wrapper">
-            {posts.map(post => (
-              <PostCard key={post.id} {...post} />
-            ))}
-          </div>
-          <div className="stories-wrapper">
-            <UserInfo
-              photoSize={58}
-              label={`${userInfo.firstName} ${userInfo.lastName}`}
-              {...userInfo}
-            />
-            <StoriesCard stories={stories} />
-            <SuggestionsCard suggestions={suggestions} />
-          </div>
-        </section>
-      </Main>
-      <GlobalStyles />
-      <style jsx>{`
-        .page-wrapper {
-          width: 100%;
-          display: flex;
-          flex-flow: row noWrap;
-          justify-content: space-between;
-          max-width: 935px;
-          padding-top: 60px;
-          margin-top: 77px;
-        }
-        .posts-wrapper {
-          width: 65%;
-        }
-        .stories-wrapper {
-          height: 100vh;
-          position: sticky;
-          right: 0;
-          top: 77px;
-          width: 100%;
-          max-width: 293px;
-        }
-        @media (max-width: 1000px) {
-          .stories-wrapper {
-            display: none;
-          }
-          .page-wrapper {
-            justify-content: center;
-          }
-        }
-      `}</style>
-    </section>
-  )
-}
+class Home extends React.Component {
+  static async getInitialProps({ req }) {
+    let posts = []
+    let userInfo = []
+    let stories = []
+    let suggestions = []
 
-Home.getInitialProps = async ({ req }) => {
-  let posts = []
-  let userInfo = []
-  let stories = []
-  let suggestions = []
-
-  if (req) {
-    posts = postsGenerator(5)
-    userInfo = userGenerator()
-    stories = storiesGenerator(10)
-    suggestions = suggestionsGenerator(3)
+    if (req) {
+      posts = postsGenerator(5)
+      userInfo = userGenerator()
+      stories = storiesGenerator(10)
+      suggestions = suggestionsGenerator(3)
+    }
+    return { posts, userInfo, stories, suggestions }
   }
-  return { posts, userInfo, stories, suggestions }
+
+  state = {
+    posts: [...this.props.posts]
+  }
+
+  componentDidMount() {
+    window.onscroll = () => {
+      const { error, isLoading } = this.state
+      if (error || isLoading) return
+
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        this.loadMorePosts()
+      }
+    }
+  }
+
+  loadMorePosts = () => {
+    this.setState({ isLoading: true }, () => {
+      const nextPosts = postsGenerator(5)
+
+      this.setState({
+        isLoading: false,
+        posts: [...this.state.posts, ...nextPosts]
+      })
+    })
+  }
+
+  render() {
+    const { posts } = this.state
+    const { userInfo, stories, suggestions } = this.props
+    return (
+      <section>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Dancing+Script"
+            rel="stylesheet"
+          />
+        </Head>
+        <Header />
+        <Main>
+          <section className="page-wrapper">
+            <div className="posts-wrapper">
+              {posts.map(post => (
+                <PostCard key={post.id} {...post} />
+              ))}
+            </div>
+            <div className="stories-wrapper">
+              <UserInfo
+                photoSize={58}
+                label={`${userInfo.firstName} ${userInfo.lastName}`}
+                {...userInfo}
+              />
+              <StoriesCard stories={stories} />
+              <SuggestionsCard suggestions={suggestions} />
+            </div>
+          </section>
+        </Main>
+        <GlobalStyles />
+        <style jsx>{`
+          .page-wrapper {
+            width: 100%;
+            display: flex;
+            flex-flow: row noWrap;
+            justify-content: space-between;
+            max-width: 935px;
+            padding-top: 60px;
+            margin-top: 77px;
+          }
+          .posts-wrapper {
+            width: 65%;
+          }
+          .stories-wrapper {
+            height: 100vh;
+            position: sticky;
+            right: 0;
+            top: 77px;
+            width: 100%;
+            max-width: 293px;
+          }
+          @media (max-width: 1000px) {
+            .stories-wrapper {
+              display: none;
+            }
+            .page-wrapper {
+              justify-content: center;
+            }
+          }
+        `}</style>
+      </section>
+    )
+  }
 }
 
 export default Home
